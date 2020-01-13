a04163f07f36f3db49606ce7028e70439499dbc2
Yan Kostadinov <yankostadinov@gmail.com>
2020-01-13 23:29:14 +0200

    fix: add local testing start script

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const fs = require('fs');
 
 const app = express();
 const database = {
